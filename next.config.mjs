c41e3dcb93901228980814d1228cce67da2ede71
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-05-20 21:25:48 +0900

    error -- dynamic routing
    동적 라우팅 접근이 안되는 에러 발생

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  basePath: "/quiz-me",
   assetPrefix: process.env.NODE_ENV === "production" ? "https://itsjh1242.github.io/quiz-me/" : "",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
